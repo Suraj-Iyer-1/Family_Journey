b0dbbab799c6b705c4082478943ac915fe14718f
Suraj-Iyer-1 <Suraj-Iyer-1@users.noreply.github.com>
2026-06-27 19:56:46 +0000

    deploy: 8a3467c4d7bcf38de636e9e9c79be1a8057f4950

--- assets/js/25aab2eb.02daeb89.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunknirvahana=self.webpackChunknirvahana||[]).push([["548"],{7178(a){a.exports=JSON.parse('{"metadata":{"permalink":"/Family_Manual/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.11d89e8b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknirvahana=self.webpackChunknirvahana||[]).push([["220"],{1912(a){a.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
